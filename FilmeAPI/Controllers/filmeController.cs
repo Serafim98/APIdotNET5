4dbe7cc9f9264c1b47f3bab207c9fa6e4f9389b3
Luis Serafim <fernandodss30@hotmail.com>
2022-08-25 11:39:51 -0300

    Iniciando conexão com banco

---
+++ FilmeAPI/Controllers/filmeController.cs
@@ -0,0 +1,38 @@
+﻿using FilmeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    
+    public class filmeController:ControllerBase
+    {
+        static int id = 1;
+        private static List<Filme> filmes = new List<Filme>();
+        [HttpPost]
+        public IActionResult addFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(recuperarFilmesPorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult recuperarFilmes()
+        {
+            return Ok(filmes);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult recuperarFilmesPorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme != null)
+                return Ok(filme);
+            return NotFound();
+        }
+    }
+}
---
+++ FilmeAPI/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmeAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo título é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo Gênero é obrigatório")]
+        [StringLength(30, ErrorMessage = "O Gênero não pode ter mais de 30 caracteres")]
+        public string Genero { get; set; }
+        public string Diretor { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração dever estar entre 1 e 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
